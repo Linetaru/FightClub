4cfbab567b5c3a0f3d0bd4948b2c8fe51cd2e106
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-01-26 18:04:56 +0100

    Start d'ecriture de l'input controller, ajout d'un prefab d'input manager venant de Rewired.

--- Assets/--Game/Scripts/Nicolas/InputController.cs
+++ Assets/--Game/Scripts/Nicolas/InputController.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class InputController : MonoBehaviour
 {
+	Rewired.Player players;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+		players = ReInput.players.GetPlayer(playerID);
 	}
 
 	// Update is called once per frame
