3989307a5918b80235ec48886d76f328cf0e6d12
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-01-27 17:48:44 +0100

    Continuation des inputs : Mapping sur Rewired InputManager + Add ControlMapper.

--- Assets/--Game/Scripts/Nicolas/InputController.cs
+++ Assets/--Game/Scripts/Nicolas/InputController.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using Rewired;
 
+public enum State{
+	Menu,
+	UI_Pause,
+	InGame,
+}
+
 public class InputController : MonoBehaviour
 {
-	Rewired.Player players;
+	List<Rewired.Player> players = new List<Player>();
+
+	int[] playerID = new int[4];
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		players = ReInput.players.GetPlayer(playerID);
+		for (int i = 0; i < playerID.Length; i++)
+		{
+			playerID[i] = i;
+			players.Add(ReInput.players.GetPlayer(i));
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
+		if(players[0].GetButtonDown("Jump"))
+        {
+			Debug.Log("Test");
+        }
 	}
+
+
 }
