42e0e94bf9b8bb3f49aa55039279736f9cae04c3
Nam <nam.mirbeau@gmail.com>
2021-01-27 16:10:11 +0100

    Début du state machine + custom rigidbody

---
+++ Assets/--Game/Scripts/Character/CharacterBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+	[SerializeField]
+	CharacterState currentState;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+
+	public void SetState(CharacterState characterState)
+	{
+		if(currentState != null)
+			currentState.EndState(this);
+		currentState = characterState;
+		currentState.StartState(this);
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		currentState.UpdateState(this);
+	}
+}
---
+++ Assets/--Game/Scripts/Character/States/CharacterState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterState : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public virtual void StartState(CharacterBase character)
+	{
+
+	}
+
+	public virtual void UpdateState(CharacterBase character)
+	{
+
+	}
+
+	public virtual void EndState(CharacterBase character)
+	{
+
+	}
+}
