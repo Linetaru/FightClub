39d9918a38876d8a0d1c813d72e8a0355c6694ba
Nam <nam.mirbeau@gmail.com>
2021-01-28 11:47:51 +0100

    Debug Stat

---
+++ Assets/--Game/Scripts/Character/Components/DebugStat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class DebugStat : MonoBehaviour
+{
+	[SerializeField]
+	Stats speed;
+
+	public float speedValue;
+
+
+	[Title("Debug")]
+	[SerializeField]
+	float value;
+	[SerializeField]
+	bool addition;
+
+	[Button("Add")]
+	public void AddStat()
+	{
+		if(addition == true)
+			speedValue = speed.UpdateStat(speed.baseStat, value, 0);
+		else
+			speedValue = speed.UpdateStat(speed.baseStat, 0, value);
+	}
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
--- Assets/--Game/Scripts/Character/Components/Stats.cs
+++ Assets/--Game/Scripts/Character/Components/Stats.cs
@@ -7,7 +7,7 @@ public class Stats : MonoBehaviour
     public float baseStat = 100.0f;
     float multiplierValue = 1.0f;
     float incrementValue = 0.0f;
-    float newStat;
+    public float newStat;
 
     public float UpdateStat(float baseStat, float collectibleIncrementValue = 0.0f, float collectibleMultiplierValue = 1.0f)
     {
