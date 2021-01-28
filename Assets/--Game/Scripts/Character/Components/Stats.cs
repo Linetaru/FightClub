10076022d4cc19984c59bc1d8a886ed3c82c5b67
Clarence <diyobe2207@gmail.com>
2021-01-28 11:39:19 +0100

    Begin to implement stats system

---
+++ Assets/--Game/Scripts/Character/Components/Stats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public float baseStat = 100.0f;
+    float multiplierValue = 1.0f;
+    float incrementValue = 0.0f;
+    float newStat;
+
+    public float UpdateStat(float baseStat, float collectibleIncrementValue = 0.0f, float collectibleMultiplierValue = 1.0f)
+    {
+        return (baseStat * (multiplierValue * collectibleIncrementValue)) + (incrementValue + collectibleIncrementValue);
+    }
+}
