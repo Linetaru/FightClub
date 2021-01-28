7729b9a0719a6504c09408900364e58e1dc90d89
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-01-28 17:23:48 +0100

    Avancement InputController + Test de divers possibilité.

---
+++ Assets/--Game/Scripts/Nicolas/InputConst.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class InputConst : ScriptableObject
+{
+	string Movement = Rewired.ReInput.mapping.GetAction(0).name;
+	string Jump = Rewired.ReInput.mapping.GetAction(1).name;
+	string Attack = Rewired.ReInput.mapping.GetAction(2).name;
+	string Action3 = Rewired.ReInput.mapping.GetAction(3).name;
+	string Action4 = Rewired.ReInput.mapping.GetAction(4).name;
+	string Interact = Rewired.ReInput.mapping.GetAction(5).name;
+	string Return = Rewired.ReInput.mapping.GetAction(6).name;
+	string Pause = Rewired.ReInput.mapping.GetAction(7).name;
+}
--- Assets/--Game/Scripts/Nicolas/InputController.cs
+++ Assets/--Game/Scripts/Nicolas/InputController.cs
@@ -9,30 +9,123 @@ public enum State{
 	InGame,
 }
 
+[System.Serializable]
+public class Input_Info
+{
+	//public Rewired.InputAction inputAction;
+	public float horizontal;
+	public float vertical;
+
+	public List<Rewired.InputAction> inputActions;
+
+	public Input_Info()
+    {
+		horizontal = 0;
+		vertical = 0;
+
+		inputActions = new List<InputAction>();
+	}
+}
+
 public class InputController : MonoBehaviour
 {
 	List<Rewired.Player> players = new List<Player>();
 
 	int[] playerID = new int[4];
 
+	//EntityController[] entityControllers;
+	//MenuController menuController;
+
+	public State state;
+
+	public Input_Info[] playerInputBuffer = new Input_Info[4];
+	public int bufferLength = 6;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(this);
+	}
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		for (int i = 0; i < playerID.Length; i++)
+		for (int i = 0; i < 4; i++)
 		{
-			playerID[i] = i;
 			players.Add(ReInput.players.GetPlayer(i));
+			playerID[i] = players[i].id;
 		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if(players[0].GetButtonDown("Jump"))
-        {
-			Debug.Log("Test");
+		for (int i = 0; i < 4; i++)
+		{
+            switch (state)
+            {
+                case State.Menu:
+					Input_Interact();
+					Input_Return();
+					break;
+                case State.UI_Pause:
+					Input_Interact();
+					Input_Return();
+					Input_Pause();
+					break;
+                case State.InGame:
+					//if (entityControllers.Length == 0)
+					//{
+					//	entityControllers = GameObject.FindObjectsOfType<EntityController>();
+					//}
+					//else
+					//{
+						Input_Movement(i);
+						Input_Jump(i);
+						Input_Attack();
+						Input_Pause();
+					//}
+					break;
+            }
         }
+    }
+
+	void Input_Movement(int ID)
+	{
+		if (Mathf.Abs(players[ID].GetAxis("Horizontal")) > .35)
+		{
+
+		}
+		else
+		{
+
+		}
+
 	}
 
+	void Input_Jump(int ID)
+	{
+		if (players[ID].GetButtonDown("Jump"))
+		{
+			playerInputBuffer[ID].inputActions.Add(ReInput.mapping.GetAction("Jump"));
+		}
+	}
 
+	void Input_Attack()
+	{
+
+	}
+	void Input_Interact()
+	{
+
+	}
+
+	void Input_Return()
+	{
+
+	}
+
+	void Input_Pause()
+	{
+
+	}
 }
