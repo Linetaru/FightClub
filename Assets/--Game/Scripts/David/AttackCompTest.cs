fe66f938abe89f223473499ff2998bdea4a4bebc
David <synlike@gmail.com>
2021-02-09 12:15:11 +0100

    AttackManager + AttackComponents

---
+++ Assets/--Game/Scripts/David/AttackCompTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackCompTest : AttackComponent
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override void OnHit()
+    {
+        Debug.Log("TestHit");
+    }
+}
---
+++ Assets/--Game/Scripts/David/AttackManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<AttackComponent> atkCompList;
+
+    public void Start()
+    {
+
+    }
+
+    public void ActionActive()
+    {
+
+    }
+
+    public void CancelAction()
+    {
+
+    }
+
+    public void Hit()
+    {
+        foreach(AttackComponent atkC in atkCompList)
+        {
+            atkC.OnHit();
+        }
+    }
+
+}
