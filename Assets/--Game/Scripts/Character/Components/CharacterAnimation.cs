916df9b702f121d8306f7b6a5dedc1a5ec89bd0d
Clarence <diyobe2207@gmail.com>
2021-02-10 18:35:48 +0100

    Wallrun animation in progress (not working)

--- Assets/--Game/Scripts/Character/Components/CharacterAnimation.cs
+++ Assets/--Game/Scripts/Character/Components/CharacterAnimation.cs
@@ -4,46 +4,95 @@ using UnityEngine;
 
 public class CharacterAnimation : MonoBehaviour
 {
-	[SerializeField]
-	CharacterBase characterBase;
+    [SerializeField]
+    CharacterBase characterBase;
 
 
-	[SerializeField]
-	Animator animator;
-	[SerializeField]
-	CharacterMovement movement;
+    [SerializeField]
+    Animator animator;
+    [SerializeField]
+    CharacterMovement movement;
 
-	[SerializeField]
-	float speedDelta = 30;
+    [SerializeField]
+    float speedDelta = 30;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		characterBase.OnStateChanged += CheckState;
-	}
+    public enum ActualState
+    {
+        Idle,
+        Knockback,
+        Wallrun
+    }
+    ActualState actualState;
 
-	public void CheckState(CharacterState oldState, CharacterState newState)
-	{
-		if (newState is CharacterStateIdle)
-			animator.SetTrigger("Idle");
-		if (newState is CharacterStateAerial)
-			animator.SetTrigger("Fall");
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterBase.OnStateChanged += CheckState;
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		float speedT = movement.SpeedX / (movement.MaxSpeed - speedDelta);
-		animator.SetFloat("Speed", Mathf.Clamp(speedT, 0, 1));
+    public void CheckState(CharacterState oldState, CharacterState newState)
+    {
+        if (newState is CharacterStateIdle)
+        {
+            animator.SetTrigger("Idle");
+            actualState = ActualState.Idle;
+        }
+        if (newState is CharacterStateAerial)
+        {
+            animator.SetTrigger("Fall");
+        }
+        if (newState is CharacterStateWallRun)
+        {
+            animator.SetTrigger("Wallrun");
+            actualState = ActualState.Wallrun;
+        }
+        if (newState is CharacterStateKnockback)
+        {
+            animator.SetTrigger("Knockback");
+            actualState = ActualState.Knockback;
+        }
+    }
 
-		if (movement.Direction == 1)
-			animator.transform.localScale = Vector3.one;
-		else if (movement.Direction == -1)
-			animator.transform.localScale = new Vector3(1,1,-1);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if(actualState == ActualState.Idle)
+        {
+            AnimationIdle();
+        }
+        else if (actualState == ActualState.Wallrun)
+        {
+            AnimationWallrun();
+        }
+        else if (actualState == ActualState.Knockback)
+        {
 
-	void OnDestroy()
-	{
-		characterBase.OnStateChanged -= CheckState;
-	}
+        }
+    }
+    void AnimationIdle()
+    {
+        float speedT = movement.SpeedX / (movement.MaxSpeed - speedDelta);
+        animator.SetFloat("Speed", Mathf.Clamp(speedT, 0, 1));
+
+        if (movement.Direction == 1)
+            animator.transform.localScale = Vector3.one;
+        else if (movement.Direction == -1)
+            animator.transform.localScale = new Vector3(1, 1, -1);
+    }
+    void AnimationWallrun()
+    {
+        if (movement.SpeedY > 0)
+        {
+            animator.SetTrigger("Wallrun");
+        }
+        else
+        {
+            animator.SetTrigger("Hanging");
+        }
+    }
+
+    void OnDestroy()
+    {
+        characterBase.OnStateChanged -= CheckState;
+    }
 }
