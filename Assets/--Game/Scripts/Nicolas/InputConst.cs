435e5e6f0dd13ec9a908d33e85bd083c2d8b6115
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-02-11 16:58:58 +0100

    Ajout d'un nouvel Input !

--- Assets/--Game/Scripts/Nicolas/InputConst.cs
+++ Assets/--Game/Scripts/Nicolas/InputConst.cs
@@ -9,7 +9,7 @@ public static class InputConst
 	public static InputAction Jump = Rewired.ReInput.mapping.GetAction(1);
 	public static InputAction Attack = Rewired.ReInput.mapping.GetAction(2);
 	public static InputAction Vertical = Rewired.ReInput.mapping.GetAction(3);
-	public static InputAction Action4 = Rewired.ReInput.mapping.GetAction(4);
+	public static InputAction Smash = Rewired.ReInput.mapping.GetAction(4);
 	public static InputAction Interact = Rewired.ReInput.mapping.GetAction(5);
 	public static InputAction Return = Rewired.ReInput.mapping.GetAction(6);
 	public static InputAction Pause = Rewired.ReInput.mapping.GetAction(7);
