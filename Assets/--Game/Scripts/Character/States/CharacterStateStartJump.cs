e408cc62be57c4fbe1ac8974ce3660ffc1777c14
David <synlike@gmail.com>
2021-02-23 10:51:11 +0100

    Character jump state creation

---
+++ Assets/--Game/Scripts/Character/States/CharacterStateStartJump.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStateStartJump : CharacterState
+{
+
+
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+		
+	}
+
+	public override void StartState(CharacterBase character, CharacterState oldState)
+	{
+
+	}
+
+	public override void UpdateState(CharacterBase character)
+	{
+
+	}
+	
+	public override void LateUpdateState(CharacterBase character)
+	{
+
+	}
+
+	public override void EndState(CharacterBase character, CharacterState newState)
+	{
+
+	}
+}
