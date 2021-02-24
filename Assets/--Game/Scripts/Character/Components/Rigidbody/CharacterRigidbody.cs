3176fb6bce1c0d9b24367b55b64973d550850cc6
Nam <nam.mirbeau@gmail.com>
2021-02-24 17:12:38 +0100

    On peut passer au travers de plateforme

--- Assets/--Game/Scripts/Character/Components/Rigidbody/CharacterRigidbody.cs
+++ Assets/--Game/Scripts/Character/Components/Rigidbody/CharacterRigidbody.cs
@@ -24,6 +24,24 @@ public class CharacterRigidbody : MonoBehaviour
         get { return true; }
     }
 
+    /// <summary>
+    /// Modifie le layerMask utilisé pour les collisions
+    /// </summary>
+    /// <param name="newLayerMask"></param>
+    /// <param name="groundLayerMask"></param>
+    public virtual void SetNewLayerMask(LayerMask newLayerMask, bool groundLayerMask = false)
+    {
+
+    }
+
+    /// <summary>
+    /// Reset le layerMask utilisé pour les collisions à celui de base
+    /// </summary>
+    public virtual void ResetLayerMask()
+    {
+
+    }
+
     public virtual void UpdateCollision(float speedX, float speedY)
     {
 
