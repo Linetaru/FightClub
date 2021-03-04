af34ea0c2ddcaf2535ca84dc1a1d28301dbff38f
Nam <nam.mirbeau@gmail.com>
2021-03-04 23:51:17 +0100

    Menu selection stage

---
+++ Assets/--Game/Scripts/Menu/MenuButtonList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Menu
+{
+	public class MenuButtonList : MonoBehaviour
+	{
+		[SerializeField]
+		Image imageButton;
+		[SerializeField]
+		TextMeshProUGUI mainText;
+		[SerializeField]
+		TextMeshProUGUI subText;
+
+		[SerializeField]
+		Animator animator;
+
+		private RectTransform rectTransform;
+		public RectTransform RectTransform
+		{
+			get { return rectTransform; }
+		}
+
+
+		public virtual void DrawButton(Sprite icon, string text, string text2)
+		{
+			imageButton.sprite = icon;
+			mainText.text = text;
+			subText.text = text2;
+		}
+
+		public virtual void SelectButton()
+		{
+			animator.SetTrigger("Selected");
+		}
+		public virtual void UnselectButton()
+		{
+			animator.SetTrigger("Unselected");
+		}
+	}
+}
---
+++ Assets/--Game/Scripts/Menu/RotationToCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationToCamera : MonoBehaviour
+{
+	[SerializeField]
+	Vector3 localPos;
+
+	[SerializeField]
+	Camera mainCamera;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//mainCamera = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.transform.localPosition = localPos;
+		this.transform.rotation = mainCamera.transform.rotation;
+	}
+}
