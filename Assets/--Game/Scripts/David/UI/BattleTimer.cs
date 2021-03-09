28d0f76600d01dab7e9c2231a7de99c4922f07bb
David <synlike@gmail.com>
2021-03-09 10:45:31 +0100

    UI Battle Timer

---
+++ Assets/--Game/Scripts/David/UI/BattleTimer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BattleTimer : MonoBehaviour
+{
+    [SerializeField]
+    private float timerInSeconds = 90;
+
+    [SerializeField]
+    private Text timerText;
+
+    private float countdownTimer;
+    private float minutes;
+    private float seconds;
+
+    private bool timesUp;
+
+
+
+    void Start()
+    {
+        countdownTimer = timerInSeconds;
+    }
+
+
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        if(!timesUp)
+        {
+            if (countdownTimer > 0)
+            {
+                countdownTimer -= Time.deltaTime;
+                DisplayTimer();
+            }
+            else
+            {
+                timesUp = true;
+                Debug.Log("End of battle");
+                // Do Something
+            }
+        }
+    }
+
+    void DisplayTimer()
+    {
+        minutes = Mathf.FloorToInt(countdownTimer / 60);
+        seconds = Mathf.FloorToInt(countdownTimer % 60);
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
