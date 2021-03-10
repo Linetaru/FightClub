7b587bb0c17ab939d782a0b8910cf7c79f2707b9
Nam <nam.mirbeau@gmail.com>
2021-03-10 14:41:56 +0100

    Début du menu de fin

---
+++ Assets/--Game/Scripts/Menu/MenuWin/MenuWinResultDrawer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Menu
+{
+	public class MenuWinResultDrawer : MonoBehaviour
+	{
+
+		[SerializeField]
+		Animator animator;
+
+
+		[SerializeField]
+		TextMeshProUGUI textPosition;
+		[SerializeField]
+		TextMeshProUGUI textControllerID;
+
+		[SerializeField]
+		TextMeshProUGUI textCharacterName;
+
+
+		public void DrawResult(int position, int controllerID)
+		{
+			textControllerID.text = controllerID + "P";
+			this.gameObject.SetActive(true);
+		}
+	}
+}
