f02a0486eb660a434805f0c30fcad39658fc1bd9
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-03-10 13:55:44 +0100

    Divers Modification, Make dirty scriptable object when you want save config.

--- Assets/--Game/Scripts/Nicolas/CameraConfig.cs
+++ Assets/--Game/Scripts/Nicolas/CameraConfig.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 using System;
 
-[Serializable]
 [CreateAssetMenu(fileName = "CameraConfig", menuName = "Camera/CameraConfig", order = 1)]
 public class CameraConfig : ScriptableObject
 {
     [Title("Config Zoom Controller")]
+    [SerializeField]
     public Vector3 config_offset;
 
+    [SerializeField]
     public float config_smoothTime = 0.5f;
 
+    [SerializeField]
     public float config_minZoom = 40f;
+    [SerializeField]
     public float config_maxZoom = 10f;
+    [SerializeField]
     public float config_zoomLimiter = 50f;
 
+    [SerializeField]
     public float config_fovForStaticScrolling = 60f;
 }
