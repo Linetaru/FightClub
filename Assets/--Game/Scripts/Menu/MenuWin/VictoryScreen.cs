3bc93770cccf7eff56617c987b8ef2cc1c432498
Nam <nam.mirbeau@gmail.com>
2021-03-11 01:38:18 +0100

    Menu win

---
+++ Assets/--Game/Scripts/Menu/MenuWin/VictoryScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Menu
+{
+	public class VictoryScreen : MonoBehaviour
+	{
+		[SerializeField]
+		float timeSkip;
+		[SerializeField]
+		PlayableDirector playableDirector;
+
+
+
+		public MenuWin circularReference; // Flemme de faire un event il est 1h du mat
+
+
+
+		public void SkipCinematic()
+		{
+			playableDirector.time = timeSkip;
+		}
+
+		public void SetToResult()
+		{
+			circularReference.SetStateResult();
+		}
+	}
+}
