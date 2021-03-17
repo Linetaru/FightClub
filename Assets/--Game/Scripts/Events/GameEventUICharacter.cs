0686fd619f2a9fc2a4370d54429851827acda9ca
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-03-17 10:06:51 +0100

    Test divers sur les Game Event

---
+++ Assets/--Game/Scripts/Events/GameEventUICharacter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PackageCreator.Event
+{
+    [CreateAssetMenu(fileName = "Game Event UICharacter", menuName = "PackageCreator/Game Event/Game Event UICharacter", order = 52)]
+    public class GameEventUICharacter : ScriptableObject
+    {
+        /// <summary>
+        /// reference all listener in this list for this Event 
+        /// </summary>
+        private readonly List<GameEventListenerUICharacter> eventListeners = new List<GameEventListenerUICharacter>();
+
+        public void Raise(CharacterBase cb_Value = null, float f_value = 0, int i_value = 0)
+        {
+            for (int i = eventListeners.Count - 1; i >= 0; i--)
+                eventListeners[i].OnEventRaised(cb_Value, f_value, i_value);
+        }
+
+        public void RegisterListener(GameEventListenerUICharacter listener)
+        {
+            if (!eventListeners.Contains(listener))
+            {
+                eventListeners.Add(listener);
+            }
+        }
+
+        public void UnregisterListener(GameEventListenerUICharacter listener)
+        {
+            if (eventListeners.Contains(listener))
+            {
+                eventListeners.Remove(listener);
+            }
+        }
+    }
+}
