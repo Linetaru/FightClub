37cb63d5acfdbe252d3e0cf86cf6c5623d3dc9fa
Clarence <diyobe2207@gmail.com>
2021-03-18 01:45:56 +0100

    Recovery in progress

---
+++ Assets/--Game/Scripts/Character/States/CharacterStateRecovery.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStateRecovery : CharacterState
+{
+	[SerializeField]
+	CharacterState aerialState;
+
+	[SerializeField]
+	GameObject recoveryAura;
+
+
+	[SerializeField]
+	float recoveryDuration = 1f;
+
+	float timer = 0f;
+
+	public override void StartState(CharacterBase character, CharacterState oldState)
+	{
+		timer = 0f;
+		recoveryAura.SetActive(true);
+	}
+
+	public override void UpdateState(CharacterBase character)
+	{
+		timer += Time.deltaTime;
+		if(timer >= recoveryDuration)
+        {
+			character.SetState(aerialState);
+        }
+	}
+	
+	public override void LateUpdateState(CharacterBase character)
+	{
+
+	}
+
+	public override void EndState(CharacterBase character, CharacterState newState)
+	{
+		recoveryAura.SetActive(false);
+	}
+}
