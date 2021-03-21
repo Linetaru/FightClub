e8fab3bd2adaecc16b14273c9186feebc4723c81
Nam <nam.mirbeau@gmail.com>
2021-03-21 16:39:25 +0100

    Test de ptit script de son

--- Assets/--Game/Scripts/Character/Components/Attack/AttackC_Sounds.cs
+++ Assets/--Game/Scripts/Character/Components/Attack/AttackC_Sounds.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using AK.Wwise;
 
-[RequireComponent(typeof(AudioSource))]
 public class AttackC_Sounds : AttackComponent
 {
+
+	[SerializeField]
+	AK.Wwise.Event soundEvent;
 
 	// Appelé au moment où l'attaque est initialisé
     public override void StartComponent(CharacterBase user)
     {
-		
-    }
+		AkSoundEngine.PostEvent(soundEvent.Id, this.gameObject);
+	}
 	
 	// Appelé tant que l'attaque existe 
 	//(Peut-être remplacé par l'Update d'Unity de base si l'ordre d'éxécution n'est pas important)
 	public override void UpdateComponent(CharacterBase user)
     {
-		
     }
 	
 	// Appelé au moment où l'attaque touche une target
