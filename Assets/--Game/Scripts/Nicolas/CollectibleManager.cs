2811bbf47bef01236249074ae6fbe2d9a137ac09
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-03-23 12:30:46 +0100

    Ajout de commentaire.

--- Assets/--Game/Scripts/Nicolas/CollectibleManager.cs
+++ Assets/--Game/Scripts/Nicolas/CollectibleManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
+//State Machine for each boost
 public enum Boost_Transition_State{
     OnStart,
     OnLife,
@@ -10,16 +11,20 @@ public enum Boost_Transition_State{
     OnTriggered,
 }
 
+//Class to add new boost spawnable on level
 [System.Serializable]
 public class Boost_Config
 {
+    //Current spawned Boost reference and Prefab boost reference
     [Title("Object")]
     [ReadOnly] public CollectibleObject currentSpawnedBoost;
     public GameObject prefabBoostObject;
 
+    //Current State of this boost
     [Title("State")]
     [ReadOnly] public Boost_Transition_State state = Boost_Transition_State.OnStart;
 
+    //All timer for each state
     [Title("Parameter")]
     public float timeOnStartBeforeSpawn;
     [HideInInspector] public float timerOnStart;
@@ -28,6 +33,7 @@ public class Boost_Config
     public float timeBeforeAnotherSpawn;
     [HideInInspector] public float timerSpawn;
 
+    //Function to check if current spawned boost as reference
     public bool IsBoostNull()
     {
         if (currentSpawnedBoost == null)
@@ -37,25 +43,32 @@ public class Boost_Config
     }
 }
 
+//Collectible manager to spawn and manage boost
 public class CollectibleManager : MonoBehaviour
 {
+    //Reference at cameraManager for access to each config
     public CameraManager cameraManager;
 
+    //List of boost
     [Title("Boost Object")]
     public Boost_Config[] boost_Configs;
 
+    //Debug Parameter
     //[Title("Parameter")]
     private int isAlreadyReset;
 
     // Update is called once per frame
     private void Update()
     {
+        //Update for each boost in array
         for(int i = 0; i < boost_Configs.Length; i++)
         {
+            //Reset Method on scrolling mode to spawn new boost on next config
             if (cameraManager.stateCamera == StateCamera.InMovingMode && isAlreadyReset == boost_Configs.Length)
             {
                 ResetBoostToNextConfig(i);
             }
+            //If Reset is done, restart value for active boost spawn
             else if(cameraManager.stateCamera == StateCamera.InFocusMode && isAlreadyReset == boost_Configs.Length)
             {
                 isAlreadyReset = 0;
@@ -63,10 +76,12 @@ public class CollectibleManager : MonoBehaviour
 
             var b = boost_Configs[i];
 
+            //If reset is not done return to not starting spawn
             if(isAlreadyReset != 0) { return; }
 
             switch (b.state)
             {
+                //Spawn a boost in Start after a time
                 case Boost_Transition_State.OnStart:
                     if (b.timerOnStart < b.timeOnStartBeforeSpawn)
                         b.timerOnStart += Time.deltaTime;
@@ -78,6 +93,7 @@ public class CollectibleManager : MonoBehaviour
                     }
                     break;
 
+                //Manage life time of boost to be destroyed if no one take it, change state if a player take before life time is over
                 case Boost_Transition_State.OnLife:
 
                     if (b.IsBoostNull())
@@ -97,6 +113,7 @@ public class CollectibleManager : MonoBehaviour
                     }
                     break;
 
+                //Timer to respawn boost after a boost taken or destroyed in time interval
                 case Boost_Transition_State.OnWaitingToSpawn:
 
                     if(b.timerSpawn < b.timeBeforeAnotherSpawn)
@@ -109,6 +126,7 @@ public class CollectibleManager : MonoBehaviour
                     }
                     break;
 
+                //Change State when we got triggered by a player
                 case Boost_Transition_State.OnTriggered:
                     b.state = Boost_Transition_State.OnWaitingToSpawn;
                     break;
@@ -116,12 +134,15 @@ public class CollectibleManager : MonoBehaviour
         }
     }
 
+    //Spawn New Boost for each config in array, Take a random platform in level in current config to fight, and instantiate randomly on this platform the prefab boost
     public void SpawnNewCollectible(int ID)
     {
-       Bounds platformBounds = cameraManager.cam_Infos[cameraManager.positionID].platform[Random.Range(0, cameraManager.cam_Infos[cameraManager.positionID].platform.Count)].GetComponent<BoxCollider>().bounds;
-       boost_Configs[ID].currentSpawnedBoost = Instantiate(boost_Configs[ID].prefabBoostObject, new Vector3(Random.Range(platformBounds.min.x, platformBounds.max.x), platformBounds.max.y + 0.5f, platformBounds.min.z + 1f), Quaternion.identity).GetComponent<CollectibleObject>();
+        //Stock bounds of a random platform for current stage config to be used to intantiate randomly on the top of this platform a boost
+        Bounds platformBounds = cameraManager.cam_Infos[cameraManager.positionID].platform[Random.Range(0, cameraManager.cam_Infos[cameraManager.positionID].platform.Count)].GetComponent<BoxCollider>().bounds;
+        boost_Configs[ID].currentSpawnedBoost = Instantiate(boost_Configs[ID].prefabBoostObject, new Vector3(Random.Range(platformBounds.min.x, platformBounds.max.x), platformBounds.max.y + 0.5f, platformBounds.min.z + 1f), Quaternion.identity).GetComponent<CollectibleObject>();
     }
 
+    //Reset all timer and state when scrolling start
     public void ResetBoostToNextConfig(int ID)
     {
         var b = boost_Configs[ID];
--- Assets/--Game/Scripts/Nicolas/CollectibleObject.cs
+++ Assets/--Game/Scripts/Nicolas/CollectibleObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
+//Type of collectible for reference
 public enum Collectible_type
 {
 	None,
@@ -10,18 +11,22 @@ public enum Collectible_type
 	MegaBoost,
 }
 
+//Entity boost with requireComponent to be able to call this components
 [RequireComponent(typeof(SphereCollider),(typeof(Rigidbody)))]
 public abstract class CollectibleObject : MonoBehaviour
 {
+	//access of boost type is it
 	[Title("Type of Collectible")]
 	public Collectible_type collectibleType;
 
+	//Method to set automaticaly when script is apply on a object
     private void Reset()
     {
 		GetComponent<SphereCollider>().isTrigger = true;
 		GetComponent<Rigidbody>().isKinematic = true;
     }
 
+	//Trigger to check which player take it and give boost at the player before destroy itself
 	public virtual void OnTriggerEnter(Collider other)
 	{
 		for (int i = 1; i < 5; i++)
