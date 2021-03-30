02a2b1653e3982b327a238e9e06aa16486a971ee
Nam <nam.mirbeau@gmail.com>
2021-03-30 17:50:55 +0200

    Training : possibilité d'enregistrer les inputs

---
+++ Assets/--Game/Scripts/Debug/DebugRegisterInput.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+
+[System.Serializable]
+public enum EnumInput
+{
+	A,
+	B,
+	X,
+	Y,
+}
+
+[System.Serializable]
+public class DebugInput
+{
+	[HorizontalGroup("Debug")]
+	[VerticalGroup("Debug/Left")]
+	[SerializeField]
+	public float frame;
+
+	[VerticalGroup("Debug/Left")]
+	[SerializeField]
+	[Range(-1, 1)]
+	public float horizontal;
+	[VerticalGroup("Debug/Left")]
+	[Range(-1, 1)]
+	[SerializeField]
+	public float vertical;
+
+	[VerticalGroup("Debug/Right")]
+	[SerializeField]
+	[HideLabel]
+	List<int> inputs;
+
+	// Pour enregistrer les inputs
+
+	public DebugInput(float timestamp, Input_Info input_Info)
+	{
+		frame = timestamp;
+		horizontal = input_Info.horizontal;
+		vertical = input_Info.vertical;
+
+
+
+		inputs = new List<int>();
+
+		if (input_Info.CheckAction(0, InputConst.Jump))
+			inputs.Add(0);
+		else if (input_Info.CheckAction(0, InputConst.Attack))
+			inputs.Add(1);
+	}
+
+
+	// Pour jouer les inputs
+	public void AssignInput(InputController inputController, ref Input_Info input)
+	{
+		inputController.AddMovement(horizontal, vertical, ref input);
+
+		for (int i = 0; i < inputs.Count; i++)
+		{
+			if (inputs[i] == 0)
+				inputController.AddInput(InputConst.Jump.name, ref input);
+			if (inputs[i] == 1)
+				inputController.AddInput(InputConst.Attack.name, ref input);
+		}
+	}
+
+	// Pour jouer les inputs
+	public void AssignInputAxis(InputController inputController, ref Input_Info input)
+	{
+		inputController.AddMovement(horizontal, vertical, ref input);
+	}
+
+}
+
+public class DebugRegisterInput : MonoBehaviour
+{
+	[SerializeField]
+	InputController inputController;
+	[SerializeField]
+	int characterID = 0;
+
+	IControllable character;
+
+	[SerializeField]
+	List<DebugInput> debugInputs;
+
+	bool registerInput = false;
+
+	bool playInput = false;
+	public float playTime = 0;
+	Input_Info inputs;
+	int indexPlay = 0;
+
+
+	// =========================================================
+	[Button]
+	public void StartRegisterInput()
+	{
+		playTime = 0f;
+		registerInput = true;
+		debugInputs.Clear();
+	}
+
+	public void RegisterInput()
+	{
+		debugInputs.Add(new DebugInput(playTime, inputController.playerInputs[characterID]));
+	}
+
+	[Button]
+	public void StopRegisterInput()
+	{
+		registerInput = false;
+	}
+
+
+
+
+
+
+	// =========================================================
+	[Button]
+	public void StartPlayInput()
+	{
+		if(inputController.controllable[characterID] == null)
+		{
+			StopPlayInput();
+			return;
+		}
+		indexPlay = 0;
+		playTime = 0f;
+		playInput = true;
+		inputs = new Input_Info();
+		character = inputController.controllable[characterID];
+		inputController.controllable[characterID] = null;
+	}
+
+	public void PlayInput()
+	{
+		if (inputs.inputActions.Count != 0)
+			inputController.UpdateTimeInBuffer(inputs.inputActions);
+		if (inputs.inputActions.Count != 0)
+			inputController.UpdateTimeInBuffer(inputs.inputActionsUP);
+
+		//bool keepPrevious = true;
+		while (playTime > debugInputs[indexPlay].frame)
+		{
+			debugInputs[indexPlay].AssignInput(inputController, ref inputs);
+			indexPlay+=1;
+			//keepPrevious = false;
+			if (indexPlay >= debugInputs.Count - 1)
+			{
+				StopPlayInput();
+				return;
+			}
+
+		}
+		/*if(keepPrevious == true) // 
+			debugInputs[indexPlay].AssignInputAxis(inputController, ref inputs);*/
+
+		//Debug.Log("Allo");
+		character.UpdateControl(0, inputs);
+
+		if (indexPlay >= debugInputs.Count)
+			StopPlayInput();
+	}
+
+	public void StopPlayInput()
+	{
+		indexPlay = 0;
+		playTime = 0f;
+		playInput = false;
+		inputs = new Input_Info();
+
+		inputController.controllable[characterID] = character;
+		character = null;
+	}
+
+
+
+	private void Update()
+	{
+		if(playInput == true)
+		{
+			playTime += 1;//Time.deltaTime;
+			PlayInput();
+		}
+		if (registerInput == true)
+		{
+			playTime += 1;//Time.deltaTime;
+			RegisterInput();
+		}
+	}
+}
---
+++ Assets/--Game/Scripts/Debug/TrainingMode.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace Menu
+{
+	public class TrainingMode : MonoBehaviour, IControllable
+	{
+		[SerializeField]
+		InputController inputController;
+		[SerializeField]
+		MenuButtonListController listEntry;
+
+
+		[Title("Scripts")]
+		[SerializeField]
+		BattleManager battleManager;
+		[SerializeField]
+		DebugTimeScale debugTimeScale;
+
+		[Title("Parameter")]
+		[SerializeField]
+		Vector2 percentageValue;
+
+		int basePercentage = 0;
+		float timeScale = 0f;
+
+		IControllable character = null;
+
+
+
+
+
+		[Title("UI")]
+		[SerializeField]
+		GameObject menuUI;
+
+		[Button]
+		private void UpdateComponents()
+		{
+			battleManager = FindObjectOfType<BattleManager>();
+			inputController = FindObjectOfType<InputController>();
+		}
+
+
+		private void Update()
+		{
+			if(inputController.playerInputs[0].CheckAction(0, InputConst.Start))
+			{
+				Debug.Log("Heho");
+				character = inputController.controllable[0];
+				inputController.controllable[0] = this;
+				ShowMenu();
+			}
+		}
+
+		public void UpdateControl(int id, Input_Info input)
+		{
+			if (listEntry.InputList(input) == true) // On s'est déplacé dans la liste
+			{
+				SelectTrainingOption(listEntry.IndexSelection);
+			}
+			/*else if (categoryEntry.InputListHorizontal(input) == true)
+			{
+				NextEncyclopedia();
+			}*/
+			else if (input.CheckAction(id, InputConst.Return) == true)
+			{
+				QuitMenu();
+			}
+		}
+
+		public void SelectTrainingOption(int id)
+		{
+			
+		}
+
+		public void QuitMenu()
+		{
+			inputController.controllable[0] = character;
+			character = null;
+			HideMenu();
+		}
+
+
+
+
+
+
+
+		private void ShowMenu()
+		{
+			menuUI.SetActive(true);
+		}
+
+		private void HideMenu()
+		{
+			menuUI.SetActive(false);
+		}
+
+
+
+	}
+}
+
