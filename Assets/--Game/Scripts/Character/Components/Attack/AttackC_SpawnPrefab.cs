9698197cc2fd4b8bee338822f03c79038df878e2
David <synlike@gmail.com>
2021-04-01 00:23:41 +0200

    Add Spawn Boombox (Attack Comp Spawn Prefab)

---
+++ Assets/--Game/Scripts/Character/Components/Attack/AttackC_SpawnPrefab.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class AttackC_SpawnPrefab : AttackComponent
+{
+
+	[SerializeField]
+	private GameObject prefabToSpawn;
+
+	// Appelé au moment où l'attaque est initialisé
+    public override void StartComponent(CharacterBase user)
+	{
+		if (prefabToSpawn != null)
+		{
+			GameObject go = Instantiate(prefabToSpawn, transform.root.position, prefabToSpawn.transform.rotation);
+			go.transform.parent = gameObject.transform.root;
+		}
+	}
+	
+	// Appelé tant que l'attaque existe 
+	//(Peut-être remplacé par l'Update d'Unity de base si l'ordre d'éxécution n'est pas important)
+	public override void UpdateComponent(CharacterBase user)
+    {
+		
+    }
+	
+	// Appelé au moment où l'attaque touche une target
+    public override void OnHit(CharacterBase user, CharacterBase target)
+    {
+		
+    }
+	
+	// Appelé au moment de la destruction de l'attaque
+    public override void EndComponent(CharacterBase user)
+    {
+		
+    }
+}
