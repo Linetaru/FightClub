26c3f230c1188832c00f98f22e0a85afba7574ab
Nam <nam.mirbeau@gmail.com>
2021-03-31 14:43:41 +0200

    Training : Perso sort des combos

---
+++ Assets/--Game/Scripts/Debug/DebugDummyBehavior.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum DummyBehavior
+{
+	Idle,
+	Jump,
+	Dodge
+}
+
+public class DebugDummyBehavior : MonoBehaviour
+{
+	[SerializeField]
+	InputController inputController;
+	[SerializeField]
+	int characterID;
+
+	CharacterBase characterBase;
+
+	Input_Info input;
+
+	//bool knockbackOn = false;
+	DummyBehavior behavior;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		input = new Input_Info();
+	}
+
+
+	public void SetBehaviorToCharacter(int id, DummyBehavior dummyBehavior)
+	{
+
+		behavior = dummyBehavior;
+		if (dummyBehavior == DummyBehavior.Idle && characterBase != null)
+		{
+			characterID = id;
+			inputController.controllable[id] = characterBase;
+			characterBase = null;
+			return;
+		}
+
+		if (inputController.controllable[id] != null)
+		{
+			characterID = id;
+			characterBase = (CharacterBase)inputController.controllable[id];
+			inputController.controllable[id] = null;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (characterBase == null)
+			return;
+
+		if (behavior == DummyBehavior.Jump)
+			BehaviorJump();
+		else if (behavior == DummyBehavior.Dodge)
+			BehaviorDodge();
+
+		characterBase.UpdateControl(0, input);
+	}
+
+
+
+
+	public void BehaviorJump()
+	{
+		if (characterBase.Knockback.KnockbackDuration > 0)
+		{
+			inputController.AddInput(InputConst.Jump.name, ref input);
+		}
+		else
+		{
+			if (input.inputActions.Count != 0)
+				inputController.UpdateTimeInBuffer(input.inputActions);
+		}
+		/*else if (characterBase.Knockback.KnockbackDuration <= 0 && knockbackOn == true)
+		{
+			inputController.AddInput(InputConst.Jump.name, ref input);
+			knockbackOn = false;
+		}*/
+	}
+
+
+	public void BehaviorDodge()
+	{
+
+	}
+
+
+
+}
