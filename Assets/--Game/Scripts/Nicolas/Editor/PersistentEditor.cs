23a354c91b18191de31eac9a010a635d9c1bf75b
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-04-01 11:43:55 +0200

    Ajout script editeur

---
+++ Assets/--Game/Scripts/Nicolas/Editor/PersistentEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class PersistentEditor
+{
+    static PersistentEditor()
+    {
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange obj)
+    {
+        //Debug.Log("New State = " + obj.ToString());
+        if (obj == PlayModeStateChange.EnteredPlayMode)
+        {
+            EditorApplication.isPlaying = false;
+            Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+}
