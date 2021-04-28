b73e5d527ace78aa63b942b5b1fa87c21ee3b9f3
Clarence <diyobe2207@gmail.com>
2021-04-28 18:42:11 +0200

    VolleyBall mode finished

---
+++ Assets/--Game/Scripts/VolleyBall/ScoreUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class ScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI redTeamText;
+
+    [SerializeField]
+    TextMeshProUGUI blueTeamText;
+
+    void UpdateScoreText(TextMeshProUGUI text, int value)
+    {
+        text.transform.DOScale(new Vector3(1.5f, 1.5f, 1.5f), .1f)
+            .OnComplete(()=> 
+            {
+                text.transform.DOScale(new Vector3(1f, 1f, 1f), .1f);
+            });
+        text.text = value.ToString();
+    }
+
+    public void UpdateRedScoreText(int value)
+    {
+        UpdateScoreText(redTeamText, value);
+    }
+
+    public void UpdateBlueScoreText(int value)
+    {
+        UpdateScoreText(blueTeamText, value);
+    }
+}
