8db1f85f6f8d121d0ba1691808d5d9af8d2a1fa3
Nam <nam.mirbeau@gmail.com>
2021-05-19 09:31:11 +0200

    Début des saves

---
+++ Assets/--Game/Scripts/Data/Saves/SaveManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveManager : MonoBehaviour
+{
+
+    private static SaveManager instance;
+    public static SaveManager Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            // Load la save la première fois que le Save Manager est appelé (donc idéalement dès qu'on lance une session du jeu)
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+
+
+    [SerializeField]
+    string saveFileName = "save";
+
+    [SerializeField]
+    SaveData saveData;
+
+
+    public void SaveFile(SaveData saveData)
+    {
+        string json = JsonUtility.ToJson(saveData);
+        string filePath = string.Format("{0}/saves/{1}.json", Application.persistentDataPath, saveFileName);
+        Debug.Log(filePath);
+        FileInfo fileInfo = new FileInfo(filePath);
+
+        if (!fileInfo.Directory.Exists)
+        {
+            Directory.CreateDirectory(fileInfo.Directory.FullName);
+        }
+        File.WriteAllText(filePath, json);
+    }
+
+
+
+
+
+
+    public bool LoadFile()
+    {
+        string filePath = string.Format("{0}/saves/{1}.json", Application.persistentDataPath, saveFileName);
+        Debug.Log(filePath);
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            JsonUtility.FromJsonOverwrite(dataAsJson, saveData);
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Assets/--Game/Scripts/Menu/MenuList.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Au cas où on a besoin d'une classe parent
+namespace Menu
+{
+    public class MenuList : MonoBehaviour, IControllable
+    {
+        [SerializeField]
+        protected MenuButtonListController listEntry;
+
+		public event Event OnStart;
+		public event EventInt OnSelected;
+		public event EventInt OnValidate;
+		public event Event OnEnd;
+
+
+
+		public virtual void UpdateControl(int id, Input_Info input)
+		{
+			if (listEntry.InputList(input) == true) // On s'est déplacé dans la liste
+			{
+				SelectEntry(listEntry.IndexSelection);
+			}
+			else if (input.CheckAction(id, InputConst.Interact) == true)
+			{
+				ValidateEntry(listEntry.IndexSelection);
+			}
+			else if (input.CheckAction(id, InputConst.Return) == true)
+			{
+				QuitMenu();
+			}
+		}
+
+
+		protected virtual void InitializeMenu()
+		{
+			OnStart?.Invoke();
+		}
+
+		protected virtual void SelectEntry(int id)
+		{
+			OnSelected?.Invoke(id);
+		}
+
+		protected virtual void ValidateEntry(int id)
+		{
+			OnValidate?.Invoke(id);
+		}
+
+		protected virtual void QuitMenu()
+		{
+			OnEnd?.Invoke();
+		}
+	}
+}
