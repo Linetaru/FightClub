1ad2c20fe44a2ebba86aabcf30fa96a4fd5613ff
Nam <nam.mirbeau@gmail.com>
2021-06-09 21:02:48 +0200

    main menu start

---
+++ Assets/--Game/Scripts/Menu/MenuMain/MenuMainGameModes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Menu
+{
+	public class MenuMainGameModes : MenuList
+	{
+		[SerializeField]
+		Transform arrow;
+		[SerializeField]
+		Transform[] buttonsPosition;
+
+
+
+
+		protected override void SelectEntry(int id)
+		{
+			base.SelectEntry(id);
+			arrow.position = buttonsPosition[id].transform.position;
+		}
+		protected override void ValidateEntry(int id)
+		{
+			base.SelectEntry(id);
+			arrow.position = buttonsPosition[id].transform.position;
+		}
+	}
+}
