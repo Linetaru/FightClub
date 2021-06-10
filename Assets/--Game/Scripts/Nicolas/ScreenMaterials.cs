b1a340475e0414ba71a1a73f68dbf1354139b43e
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-06-10 10:53:08 +0200

    Modification divers

--- Assets/--Game/Scripts/Nicolas/ScreenMaterials.cs
+++ Assets/--Game/Scripts/Nicolas/ScreenMaterials.cs
@@ -6,6 +6,9 @@ public class ScreenMaterials : MonoBehaviour
 {
     public Material material1; 
     public Material material2;
+    public Material material3;
+    public Material material4;
+    public Material material5;
     public List<Material> textures;
     int random = 0;
     int index = 0;
@@ -15,12 +18,30 @@ public class ScreenMaterials : MonoBehaviour
     void Start()
     {
         random = Random.Range(0, 11);
-        //random = 5;
+        //random = 7;
 
         if(random == 5)
         {
             Material[] mats = this.GetComponent<MeshRenderer>().materials;
             mats[1] = material2;
+            this.GetComponent<MeshRenderer>().materials = mats;
+        }
+        else if(random == 2)
+        {
+            Material[] mats = this.GetComponent<MeshRenderer>().materials;
+            mats[1] = material3;
+            this.GetComponent<MeshRenderer>().materials = mats;
+        }
+        else if (random == 7)
+        {
+            Material[] mats = this.GetComponent<MeshRenderer>().materials;
+            mats[1] = material4;
+            this.GetComponent<MeshRenderer>().materials = mats;
+        }
+        else if (random == 9 || random == 10 || random == 8)
+        {
+            Material[] mats = this.GetComponent<MeshRenderer>().materials;
+            mats[1] = material5;
             this.GetComponent<MeshRenderer>().materials = mats;
         }
         else
