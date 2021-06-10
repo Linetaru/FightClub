70cfd90042cfcd59e32171bdc092b8a7e5a1402e
Nicolas Rogé <nicolas.roge.contact@gmail.com>
2021-06-10 12:59:46 +0200

    Update Screen Sponsor script

--- Assets/--Game/Scripts/Nicolas/ScreenMaterials.cs
+++ Assets/--Game/Scripts/Nicolas/ScreenMaterials.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class ScreenMaterials : MonoBehaviour
 {
-    public Material material1; 
-    public Material material2;
-    public Material material3;
-    public Material material4;
-    public Material material5;
-    public Material material6;
-    public Material material7;
-    public Material material8;
-    public Material material9;
     public List<Material> textures;
+    public List<Material> materialScreen;
     int random = 0;
     int index = 0;
     float timerMax = 0.1f;
@@ -21,69 +13,18 @@ public class ScreenMaterials : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        random = Random.Range(0, 11);
+        random = Random.Range(0, materialScreen.Count);
         //random = 9;
 
-        if (random == 5)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material2;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 2)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material3;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 3)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material7;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 8)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material8;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 9)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material9;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 7)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material4;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 4)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material6;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else if (random == 10)
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material5;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
-        else
-        {
-            Material[] mats = this.GetComponent<MeshRenderer>().materials;
-            mats[1] = material1;
-            this.GetComponent<MeshRenderer>().materials = mats;
-        }
+        Material[] mats = this.GetComponent<MeshRenderer>().materials;
+        mats[1] = materialScreen[random];
+        this.GetComponent<MeshRenderer>().materials = mats;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (random == 5)
+        if (random == 1)
         {
             if (timerMax <= 0)
             {
